8272ad9fa764e901fa658b175c59bb871005bd62
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-12-03 00:49:41 +0700

    🐛 fix(scripts): wrong method to set year

--- assets/scripts/updateYearly.sh
+++ assets/scripts/updateYearly.sh
@@ -5,25 +5,26 @@ get_current_year() {
     date +'%Y'
 }
 
-# Function to extract the year from the Markdown file
-get_markdown_year() {
-    grep -oP '(?<=\?username=PunGrumpy&year=)\d+' README.md
-}
-
 # Replace the year in the Markdown file if it's not the current year
 update_year_in_file() {
     current_year=$(get_current_year)
-    markdown_year=$(get_markdown_year)
     file_path="README.md"
 
     # Check if the file exists
     if [ -f "$file_path" ]; then
-        if [ "$current_year" != "$markdown_year" ]; then
-            # Replace the year in the file using sed command
-            sed -i "s/\(username=PunGrumpy&year=\)$markdown_year/\1$current_year/g" "$file_path"
-            echo "Year updated to $current_year in $file_path"
+        # Get the year mentioned in the file
+        markdown_year=$(grep -oP '(?<=© )[0-9]+' "$file_path")
+
+        if [ -n "$markdown_year" ]; then
+            if [ "$current_year" != "$markdown_year" ]; then
+                # Replace the year in the file using sed command
+                sed -i "s/© $markdown_year/© $current_year/g" "$file_path"
+                echo "Year updated to $current_year in $file_path"
+            else
+                echo "The year in $file_path is already up to date."
+            fi
         else
-            echo "The year in $file_path is already up to date."
+            echo "No year found in the specified format in $file_path."
         fi
     else
         echo "File $file_path not found."
